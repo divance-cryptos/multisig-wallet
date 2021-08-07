7c7ad2bcc626f8c7e1f43aaea0bf3b2f64d42dc0
Josep Servat <jservat@outlook.es>
2021-08-07 22:49:42 +0200

    Setup MultiSig Client

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Mutlsig Wallet
+       </header>
+    </div>
+  );
+}
+
+export default App;

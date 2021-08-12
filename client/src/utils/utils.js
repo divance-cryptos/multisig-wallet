4268e3563e0bb63a38df006ccc3ad01fbdfba073
Josep Servat <jservat@outlook.es>
2021-08-13 01:13:03 +0200

    Update metamask and solidity version

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,39 +1,32 @@
-import Web3 from 'web3';
-import Wallet from '../contracts/Wallet.json';
+import Web3 from "web3";
+import Wallet from "../contracts/Wallet.json";
+import detectEthereumProvider from "@metamask/detect-provider";
 
-const getWeb3 = () => {
+const getWeb3 = () =>
+  new Promise(async (resolve, reject) => {
+    let provider = await detectEthereumProvider();
 
-    return new Promise((resolve, reject) => {
-        window.addEventListener('load', async () => {
-            if (window.ethereum) {
-                const web3 = new Web3(window.ethereum);
-                try {
-                    await window.ethereum.enable();
-                    resolve(web3);
-                } catch (error) {
-                    reject(error);
-                }
-            } else if (window.web3) {
-                resolve(window.web3);
-            } else {
-                reject('Must Install Metamask');
-            }
-        })
-    })
+    if (provider) {
+      await provider.request({ method: "eth_requestAccounts" });
 
+      try {
+        const web3 = new Web3(window.ethereum);
+        resolve(web3);
+      } catch (error) {
+        reject(error);
+      }
+    }
+    reject("Install Metamask");
+  });
+
+const getWallet = async (web3) => {
+  const networkId = await web3.eth.net.getId();
+  const contractDeployment = Wallet.networks[networkId];
+
+  return new web3.eth.Contract(
+    Wallet.abi,
+    contractDeployment && contractDeployment.address
+  );
 };
 
-const getWallet = async web3 => {
-    const networkId = await web3.eth.net.getId();
-    const contractDeployment = Wallet.networks[networkId];
-
-    return new web3.eth.Contract(
-        Wallet.abi,
-        contractDeployment && contractDeployment.address
-    );
-}
-
-export {
-    getWeb3,
-    getWallet
-}
+export { getWeb3, getWallet };
